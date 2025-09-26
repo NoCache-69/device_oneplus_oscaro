a9eeea0dc4fd8977649e4fbe9f85350e69392734
NoCache-69 <chethangowdasj86@gmail.com>
2025-09-26 15:11:25 +0000

    oscaro: Update vendorsetup.sh
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Clone device specific repos
-git clone https://github.com/NoCache-69/device_oneplus_sm6375-common.git -b 16.0 device/oneplus/sm6375-common
+git clone https://github.com/NoCache-69/device_oneplus_sm6375-common.git -b matrixx device/oneplus/sm6375-common
 git clone https://github.com/NoCache-69/vendor_oneplus_oscaro.git -b 16.0 vendor/oneplus/oscaro
 git clone https://github.com/NoCache-69/vendor_oneplus_sm6375-common.git -b 16.0 vendor/oneplus/sm6375-common
 git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16.0 kernel/oneplus/sm6375
