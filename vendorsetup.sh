66d2ee01d95d0646355ba38039d96ff9649c991e
NoCache-69 <chethangowdasj86@gmail.com>
2025-10-21 05:52:35 +0000

    oscaro: switch kernel branch to 16-ksun

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/NoCache-69/device_oneplus_sm6375-common.git -b 16.0 device/oneplus/sm6375-common
 git clone https://github.com/NoCache-69/vendor_oneplus_oscaro.git -b 16.0 vendor/oneplus/oscaro
 git clone https://github.com/NoCache-69/vendor_oneplus_sm6375-common.git -b 16.0 vendor/oneplus/sm6375-common
-git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16.0 kernel/oneplus/sm6375
+git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16-ksun kernel/oneplus/sm6375
 git clone https://github.com/NoCache-69/android_hardware_oplus.git -b lineage-22.2 hardware/oplus
 git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git -b sony-1.1 hardware/dolby
 git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
