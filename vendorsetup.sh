1cf014ee0e3760f9006597ce4a86f75d126ffe61
NoCache-69 <chethangowdasj86@gmail.com>
2026-01-12 14:48:39 +0000

    oscaro: vendorsetup: Updated tracking for some repos - dolby: Track own fork of new dolby impl - Track lineage-23.1 branch for hw/oplus & v4a

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,18 +2,11 @@
 git clone https://github.com/NoCache-69/device_oneplus_sm6375-common.git -b lunaris device/oneplus/sm6375-common
 git clone https://github.com/NoCache-69/vendor_oneplus_oscaro.git -b 16.0 vendor/oneplus/oscaro
 git clone https://github.com/NoCache-69/vendor_oneplus_sm6375-common.git -b 16.0 vendor/oneplus/sm6375-common
-git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16-ksun kernel/oneplus/sm6375
-git clone https://github.com/NoCache-69/android_hardware_oplus.git -b lineage-22.2 hardware/oplus
-git clone https://github.com/swiitch-OFF-Lab/hardware_dolby.git -b sony-1.1 hardware/dolby
-git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16.0 kernel/oneplus/sm6375
+git clone https://github.com/NoCache-69/hardware_oplus.git -b lineage-23.1 hardware/oplus
+git clone https://github.com/NoCache-69/hardware_dolby.git -b master hardware/dolby
+git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git -b lineage-23.1 packages/apps/ViPER4AndroidFX
 git clone https://github.com/NoCache-69/packages_apps_prebuilt-camera.git packages/apps/prebuilt
-
-# Initialize KSU-Next
-cd kernel/oneplus/sm6375
-git submodule init
-git submodule update
-bash setup*
-cd ../../..
 
 # Sign Build
 git clone https://github.com/Lunaris-AOSP/vendor_lunaris-priv_keys.git vendor/lunaris-priv/keys
