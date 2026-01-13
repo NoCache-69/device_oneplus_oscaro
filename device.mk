5ecaf8061c6d63f1b83a4b3cb7aaf9d7e5a41072
NoCache-69 <chethangowdasj86@gmail.com>
2026-01-13 06:07:34 +0000

    oscaro: Include OOS camera - Drop prebuilt LMC cam

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \
@@ -65,9 +68,6 @@ PRODUCT_PACKAGES += \
 # RemovePackages
  PRODUCT_PACKAGES += \
      RemovePackagesOscaro
-# Include Gcam
- PRODUCT_PACKAGES += \
-    LMC-Prebuilt
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
