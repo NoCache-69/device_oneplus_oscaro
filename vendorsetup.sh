bf9907e646a4401cf2f22d405572998d3b624f62
NoCache-69 <chethangowdasj86@gmail.com>
2026-01-19 23:05:54 +0000

    3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,6 +3,6 @@ git clone https://github.com/NoCache-69/device_oneplus_sm6375-common.git -b 16.0
 git clone https://github.com/NoCache-69/vendor_oneplus_oscaro.git -b 16.0 vendor/oneplus/oscaro
 git clone https://github.com/NoCache-69/vendor_oneplus_sm6375-common.git -b 16.0 vendor/oneplus/sm6375-common
 git clone https://github.com/NoCache-69/android_kernel_oneplus_sm6375.git -b 16.0 kernel/oneplus/sm6375
-git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-23.2 hardware/oplus
+git clone https://github.com/NoCache-69/android_hardware_oplus.git -b 16.2 hardware/oplus
 
 export SKIP_ABI_CHECKS=true
